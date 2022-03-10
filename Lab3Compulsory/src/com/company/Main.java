f43c470c534c510a645a340f79562c077bafcc2c
Sarghe-Andrei-Vlad <sargheandreivlad@gmail.com>
2022-03-10 17:26:03 +0200

    Update Main.java

--- Lab3Compulsory/src/com/company/Main.java
+++ Lab3Compulsory/src/com/company/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         //v1 (Computer A) v2 (Router A) v3 (Switch A) v4 (Switch B) v5 (Router B) v6 (Computer B).
-        Computer v1 = new Computer("A", "1", 100);
-        Router v2 = new Router("A", "2");
-        Switch v3 = new Switch("A","3");
+        Node v1 = new Computer("A", "1", 100);
+        Node v2 = new Router("A", "2");
+        Node v3 = new Switch("A","3");
 
         v1.show();
         v2.show();
